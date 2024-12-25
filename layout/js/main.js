258a23f0f8ebe5be0c3885e25cba8dd92773483c
IslamAlsayed <eslamalsayed8133@gmail.com>
2024-12-25 20:09:07 +0200

    هmprovements are complete

--- layout/js/main.js
+++ layout/js/main.js
@@ -85,3 +85,49 @@ if (componentFooter) {
             componentFooter.innerHTML = result;
         }).catch(err => console.log(err));
 }
+
+
+let toggleQuestions = document.querySelectorAll(".toggle-questions");
+if (toggleQuestions.length > 0) {
+    toggleQuestions.forEach(toggle => {
+        toggle.addEventListener("click", () => {
+            toggle.classList.toggle("fa-plus");
+            toggle.classList.toggle("fa-minus");
+            toggle.parentElement.nextElementSibling.classList.toggle("show");
+        })
+    })
+}
+
+let fasterServicesCards = document.getElementById("faster-services-cards");
+if (fasterServicesCards) {
+    fetch("./data/faster-services.json").then(response => response.json())
+        .then(result => {
+            result.forEach(card => {
+                let cardHTML = `
+                    <div class="card" data-index="${card.id}">
+                        <div class="image">
+                        <img src="${card.image}" alt="${card.title}" />
+                        </div>
+                        <h2>${card.title}</h2>
+                    </div>
+                `;
+                fasterServicesCards.innerHTML += cardHTML;
+            })
+        }).catch(err => console.log(err));
+}
+
+function lengthText(text) {
+    if (text.length > 100) {
+        return text.substring(0, 100) + "...";
+    }
+
+    return text;
+}
+
+let elementLengths = document.querySelectorAll(".elementLength");
+if (elementLengths.length > 0) {
+    elementLengths.forEach(element => {
+        let length = element.dataset.length;
+        element.textContent = lengthText(length);
+    })
+}
--- layout/js/slider.js
+++ layout/js/slider.js
@@ -1,4 +1,4 @@
-function createSwiper(selector, paginationClass, nextClass, prevClass, breakpoints) {
+function createSwiper(selector, paginationClass, nextClass, prevClass, startingClass, breakpoints) {
     return new Swiper(selector, {
         loop: true,
         spaceBetween: 30,
@@ -11,31 +11,32 @@ function createSwiper(selector, paginationClass, nextClass, prevClass, breakpoin
             nextEl: nextClass,
             prevEl: prevClass
         },
-        breakpoints: breakpoints
+        breakpoints: breakpoints,
+        initialSlide: startingClass
     });
 }
 
-createSwiper('.preview-card-wrapper', '.swiper-pagination', '.swiper-button-next', '.swiper-button-prev', {
+createSwiper('.main-card-wrapper', '.swiper-pagination', '.swiper-button-next', '.swiper-button-prev', 1, {
     0: { slidesPerView: 1 },
-    768: { slidesPerView: 1.5 },
-    1024: { slidesPerView: 2 }
+    768: { slidesPerView: 1 },
+    1024: { slidesPerView: 1 }
 });
 
-createSwiper('.services-card-wrapper', '.services-pagination', '.services-next', '.services-prev', {
-    0: { slidesPerView: 1 },
-    768: { slidesPerView: 2 },
-    992: { slidesPerView: 3 },
-    1024: { slidesPerView: 3 }
-});
-
-createSwiper('.features-card-wrapper', '.features-pagination', '.features-next', '.features-prev', {
+createSwiper('.services-card-wrapper', '.services-pagination', '.services-next', '.services-prev', 0, {
     0: { slidesPerView: 1 },
     768: { slidesPerView: 2 },
     992: { slidesPerView: 3 },
     1024: { slidesPerView: 3 }
 });
 
-createSwiper('.projects-card-wrapper', '.projects-pagination', '.projects-next', '.projects-prev', {
+createSwiper('.features-card-wrapper', '.features-pagination', '.features-next', '.features-prev', 0, {
+    0: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    992: { slidesPerView: 3 },
+    1024: { slidesPerView: 3 }
+});
+
+createSwiper('.projects-card-wrapper', '.projects-pagination', '.projects-next', '.projects-prev', 0, {
     0: { slidesPerView: 1 },
     500: { slidesPerView: 2 },
     768: { slidesPerView: 3 },
@@ -43,7 +44,7 @@ createSwiper('.projects-card-wrapper', '.projects-pagination', '.projects-next',
     1024: { slidesPerView: 4 }
 });
 
-createSwiper('.clients-card-wrapper', '.clients-pagination', '.clients-next', '.clients-prev', {
+createSwiper('.clients-card-wrapper', '.clients-pagination', '.clients-next', '.clients-prev', 0, {
     0: { slidesPerView: 1 },
     500: { slidesPerView: 2 },
     768: { slidesPerView: 3 },
@@ -51,7 +52,7 @@ createSwiper('.clients-card-wrapper', '.clients-pagination', '.clients-next', '.
     1024: { slidesPerView: 4 }
 });
 
-createSwiper('.client-reviews-card-wrapper', '.client-reviews-pagination', '.client-reviews-next', '.client-reviews-prev', {
+createSwiper('.client-reviews-card-wrapper', '.client-reviews-pagination', '.client-reviews-next', '.client-reviews-prev', 0, {
     0: { slidesPerView: 1 },
     500: { slidesPerView: 2 },
     768: { slidesPerView: 3 },
